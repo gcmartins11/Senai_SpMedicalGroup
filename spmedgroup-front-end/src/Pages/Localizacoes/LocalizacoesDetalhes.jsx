506de45b5cf1ba4568a3d4489d955169bbe34df1
Gabriel Cardoso <gabriel.cmartins11@gmail.com>
2019-06-13 00:15:52 -0300

    Cadastro de prontuarios feito

--- spmedgroup-front-end/src/Pages/Localizacoes/LocalizacoesDetalhes.jsx
+++ spmedgroup-front-end/src/Pages/Localizacoes/LocalizacoesDetalhes.jsx
@@ -13,17 +13,26 @@ export default class LocalizacoesDetalhes extends Component {
             endereco: '',
             numero: '',
             especialidades: [],
+            prontuarios: [],
+            cadastroNome: '',
+            cadastroIdade: '',
+            cadastroLatitude: '',
+            cadastroLongitude: '',
+            cadastroDiagnostico: '',
+            
+
         }
     }
 
     componentWillMount() {
         this.buscarClinica()
-        this.buscarUsuarios()
+        this.buscarProntuarios()
     }
 
     buscarClinica() {
         firebase.firestore().collection("clinicas")
-            .where("cep", "==", localStorage.getItem("cep")).get()
+            .where("cep", "==", localStorage.getItem("cep"))
+            .get()
             .then((clinicas) => {
                 let clinicasArray = []
                 clinicas.forEach((clinica) => {
@@ -42,17 +51,51 @@ export default class LocalizacoesDetalhes extends Component {
                 this.setState({ numero: clinicasArray[0].numero })
                 this.setState({ cep: clinicasArray[0].numero })
                 this.setState({ especialidades: clinicasArray[0].especialidades })
-                
+
             })
     }
 
-    buscarUsuarios() {
-        api.get('/api/usuarios', {
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": "Bearer " + localStorage.getItem("usuario")
-            }
-        }).then(data => console.log(data))
+
+    buscarProntuarios() {
+        firebase.firestore().collection("pacientes")
+            .get()
+            .then((pacientes) => {
+                let prontuariosArray = []
+                pacientes.forEach((paciente) => {
+                    prontuariosArray.push({
+                        id: paciente.id,
+                        nome: paciente.data().nome,
+                        idade: paciente.data().idade,
+                        endereco: paciente.data().endereco,
+                        numero: paciente.data().numero,
+                        diagnostico: paciente.data().diagnostico
+                    })
+                })
+
+                this.setState({ prontuarios: prontuariosArray })
+            })
+    }
+
+    atualizarState(event) {
+        this.setState({[event.target.name]: event.target.value})
+    }
+
+    cadastrarProntuario(event) {
+        event.preventDefault()
+
+        const paciente = {
+            nome: this.state.cadastroNome,
+            idade: this.state.cadastroIdade,
+            latitude: this.state.cadastroLatitude,
+            lognitude: this.state.cadastroLongitude,
+            diagnostico: this.state.cadastroDiagnostico, 
+        }
+
+        firebase.firestore()
+        .collection("pacientes")
+        .add(paciente)
+        .then((result) => {alert('Cadastrado')})
+
     }
 
     render() {
@@ -66,7 +109,7 @@ export default class LocalizacoesDetalhes extends Component {
                         <h3 style={{ marginTop: "20px" }}>Atendimentos feitos</h3>
                         <ul style={{ margin: 0 }}>
                             {this.state.especialidades.map(key => {
-                            return <li>{key}</li>
+                                return <li>{key}</li>
                             })}
                         </ul>
                     </div>
@@ -77,6 +120,26 @@ export default class LocalizacoesDetalhes extends Component {
                         </center>
                     </div>
                 </section>
+
+                <div className="lista-prontuarios">
+                    <h1>Prontuarios</h1>
+                    {this.state.prontuarios.map((paciente) => {
+                        return <h3>{paciente.nome}</h3>
+                    })}
+                </div>
+                <div className="cadastro-Prontuarios">
+                    <h1>Cadastrar prontuario</h1>
+                    <center>
+                        <form onSubmit={this.cadastrarProntuario.bind(this)}>
+                            <input name="cadastroNome" onChange={this.atualizarState.bind(this)} placeholder="Nome" type="text" />
+                            <input name="cadastroIdade" onChange={this.atualizarState.bind(this)} placeholder="Idade" type="text" />
+                            <input name="cadastroLatitude" onChange={this.atualizarState.bind(this)} placeholder="Latitude" type="text" />
+                            <input name="cadastroLongitude" onChange={this.atualizarState.bind(this)} placeholder="Longitude" type="text" />
+                            <input name="cadastroDiagnostico" onChange={this.atualizarState.bind(this)} placeholder="Diagnostico" type="text" />
+                            <input type="submit" value="Cadastrar prontuario" />
+                        </form>
+                    </center>
+                </div>
             </div>
         )
     }
